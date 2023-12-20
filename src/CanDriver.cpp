e8b53c062a9cfca3ebe448eb93442f0321a909f8
machinetherapist <79029454+illegal-instruction-co@users.noreply.github.com>
2023-12-20 16:05:15 +0300

    Update CanDriver.cpp

--- src/CanDriver.cpp
+++ src/CanDriver.cpp
@@ -161,7 +161,7 @@ namespace sockcanpp {
 
         if (forceExtended || ((uint32_t)message.getCanId() > CAN_SFF_MASK)) { canFrame.can_id |= CAN_EFF_FLAG; }
 
-        // if (forceExtended || (message.getCanId() > CAN_SFF_MASK)) { canFrame.can_id |= CAN_EFF_FLAG; }
+        if (forceExtended || (message.getCanId() > CAN_SFF_MASK)) { canFrame.can_id |= CAN_EFF_FLAG; }
 
         bytesWritten = write(_socketFd, (const void*)&canFrame, sizeof(canFrame));
 
